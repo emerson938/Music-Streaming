f1812252c14ecbd0f7a2afbd2f5c442266daad5e
emerson938 <emersonruan150@gmail.com>
2026-03-30 18:13:19 -0300

    feat: create User model

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
+const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+const User = require("./models/User"); // 👈 corrigido
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// conectar ao banco
 connectDB();
 
 app.use(cors());
@@ -13,6 +17,20 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("Backend funcionando!");
+});
+
+app.get("/create-user", async (req, res) => {
+    try {
+        const user = await User.create({
+            name: "Teste",
+            email: "teste@email.com",
+            password: "123456"
+        });
+
+        res.json(user);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
 });
 
 app.listen(PORT, () => {
